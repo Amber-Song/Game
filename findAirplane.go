f5fc5262f4f119c56a2624c827068ee4a24020f3
Amber Song <jihongsongnz@gmail.com>
2020-06-22 20:44:12 +1200

    Fix bug that no cookie is assigned

--- findAirplane.go
+++ findAirplane.go
@@ -7,6 +7,8 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -314,4 +316,93 @@ func (game *AirplaneGame) getUpdateGameData(r *http.Request, userNow string, boa
 	}
 
 	return newGameState
+}
+
+func airplaneInitHandler(w http.ResponseWriter, r *http.Request) {
+	// Enabling CORS on a Go Web Server
+	setupResponse(w)
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	boardLength, err := strconv.Atoi(strings.Join(r.URL.Query()["boardLength"], ""))
+	if err != nil {
+		boardLength = 8
+	}
+	airplaneType := 0
+
+	user, _ := getCookie(r)
+	if user == "" {
+		user = setCookie(w)
+	}
+	roomid := setRoom()
+	board := generateAirplane(boardLength, airplaneTypes[airplaneType].AirplaneBody, airplaneTypes[airplaneType].AirplaneWing, airplaneTypes[airplaneType].AirplaneTail)
+
+	room := AirplaneRoom{player1: user, player2: "", expire: time.Now().AddDate(0, 0, 1), boardLength: boardLength, airplaneType: airplaneType}
+	game := AirplaneGame{Board1: board, Board2: board, PlayerNow: "player1", Round: 1, Win: []string{}, ThisPlayer: ""}
+	airplaneRooms[roomid] = room
+	airplaneGames[roomid] = game
+
+	cleanRooms()
+
+	b, err := json.Marshal(roomid)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write(b)
+}
+
+func airplaneGameHandler(w http.ResponseWriter, r *http.Request) {
+	// Enabling CORS on a Go Web Server
+	setupResponse(w)
+	if r.Method == "OPTIONS" {
+		return
+	}
+
+	// Get room and cookie
+	roomid := strings.Join(r.URL.Query()["room"], "")
+	check := checkRoom(roomid)
+	if !check {
+		http.Redirect(w, r, "/NotFound", http.StatusFound)
+		return
+	}
+	user, _ := getCookie(r)
+	if user == "" {
+		user = setCookie(w)
+	}
+
+	// Check what is the user now and also if is a new user, update room and game
+	room := airplaneRooms[roomid]
+	game := airplaneGames[roomid]
+	userNow := room.getUserNow(user) // this should decide which is this play instead of this player in the struct
+	if userNow == "" {
+		if room.player2 == "" {
+			userNow = "player2"
+			board := generateAirplane(room.boardLength, airplaneTypes[room.airplaneType].AirplaneBody, airplaneTypes[room.airplaneType].AirplaneWing, airplaneTypes[room.airplaneType].AirplaneTail)
+			updateRoom := AirplaneRoom{player1: room.player1, player2: user, expire: room.expire, boardLength: room.boardLength, airplaneType: room.airplaneType}
+			updateGame := AirplaneGame{Board1: game.Board1, Board2: board, PlayerNow: game.PlayerNow, Round: game.Round, Win: game.Win, ThisPlayer: userNow}
+			airplaneRooms[roomid] = updateRoom
+			airplaneGames[roomid] = updateGame
+			room = updateRoom
+			game = updateGame
+		} else {
+			http.Redirect(w, r, "/FindAirplane/Game", http.StatusFound)
+			return
+		}
+	}
+
+	// Check if it is post or get
+	if r.Method == http.MethodPost {
+		//	if post, get new data, update it and then send the data back
+		updateGame := game.getUpdateGameData(r, userNow, room.boardLength)
+		airplaneGames[roomid] = updateGame
+	} else {
+		//	if get, then return data
+		updateGame := AirplaneGame{Board1: game.Board1, Board2: game.Board2, PlayerNow: game.PlayerNow, Round: game.Round, Win: game.Win, ThisPlayer: userNow}
+		b, err := json.Marshal(updateGame)
+		if err != nil {
+			println(err)
+		}
+		w.Write(b)
+	}
 }
--- server.go
+++ server.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -16,6 +14,9 @@ import (
 var airplaneRooms map[string]AirplaneRoom
 var airplaneGames map[string]AirplaneGame
 var airplaneTypes []AirplaneType
+
+// ErrCookieNotSet is a error indicate that cookie is not set
+var ErrCookieNotSet error = errors.New("Cookie didn't set")
 
 // setupResponse() is to deal with CORS Cross Origin Resource Sharing
 /* CORS does pre-flight requests sending an OPTIONS request to any URL,
@@ -62,7 +63,7 @@ func getCookie(r *http.Request) (string, error) {
 		return "", err
 	}
 	if cookie == nil {
-		return "", errors.New("Cookie didn't set")
+		return "", ErrCookieNotSet
 	}
 	return cookie.Value, nil
 }
@@ -77,6 +78,13 @@ func setRoom() string {
 	return ""
 }
 
+func checkRoom(roomid string) bool {
+	if _, ok := airplaneRooms[roomid]; ok {
+		return true
+	}
+	return false
+}
+
 // cleanRooms remove all the expire room from games list
 func cleanRooms() {
 	for roomid, room := range airplaneRooms {
@@ -84,98 +92,6 @@ func cleanRooms() {
 			delete(airplaneRooms, roomid)
 			delete(airplaneGames, roomid)
 		}
-	}
-}
-
-func airplaneInitHandler(w http.ResponseWriter, r *http.Request) {
-	// Enabling CORS on a Go Web Server
-	setupResponse(w)
-	if r.Method == "OPTIONS" {
-		return
-	}
-
-	boardLength, err := strconv.Atoi(strings.Join(r.URL.Query()["boardLength"], ""))
-	if err != nil {
-		boardLength = 8
-	}
-	airplaneType := 0
-
-	user, err := getCookie(r)
-	if user == "" {
-		if err == errors.New("Cookie didn't set") {
-			user = setCookie(w)
-		} else {
-			fmt.Println(err)
-		}
-	}
-	roomid := setRoom()
-	board := generateAirplane(boardLength, airplaneTypes[airplaneType].AirplaneBody, airplaneTypes[airplaneType].AirplaneWing, airplaneTypes[airplaneType].AirplaneTail)
-
-	room := AirplaneRoom{player1: user, player2: "", expire: time.Now().AddDate(0, 0, 1), boardLength: boardLength, airplaneType: airplaneType}
-	game := AirplaneGame{Board1: board, Board2: board, PlayerNow: "player1", Round: 1, Win: []string{}, ThisPlayer: ""}
-	airplaneRooms[roomid] = room
-	airplaneGames[roomid] = game
-
-	cleanRooms()
-
-	b, err := json.Marshal(roomid)
-	if err != nil {
-		fmt.Println(err)
-	}
-	w.Write(b)
-}
-
-func airplaneGameHandler(w http.ResponseWriter, r *http.Request) {
-	// Enabling CORS on a Go Web Server
-	setupResponse(w)
-	if r.Method == "OPTIONS" {
-		return
-	}
-
-	// Get room and cookie
-	roomid := strings.Join(r.URL.Query()["room"], "")
-	user, err := getCookie(r)
-	if user == "" {
-		if err == errors.New("Cookie didn't set") {
-			user = setCookie(w)
-		} else {
-			fmt.Println(err)
-		}
-	}
-
-	// Check what is the user now and also if is a new user, update room and game
-	room := airplaneRooms[roomid]
-	game := airplaneGames[roomid]
-	userNow := room.getUserNow(user) // this should decide which is this play instead of this player in the struct
-	if userNow == "" {
-		if room.player2 == "" {
-			userNow = "player2"
-			board := generateAirplane(room.boardLength, airplaneTypes[room.airplaneType].AirplaneBody, airplaneTypes[room.airplaneType].AirplaneWing, airplaneTypes[room.airplaneType].AirplaneTail)
-			updateRoom := AirplaneRoom{player1: room.player1, player2: user, expire: room.expire, boardLength: room.boardLength, airplaneType: room.airplaneType}
-			updateGame := AirplaneGame{Board1: game.Board1, Board2: board, PlayerNow: game.PlayerNow, Round: game.Round, Win: game.Win, ThisPlayer: userNow}
-			airplaneRooms[roomid] = updateRoom
-			airplaneGames[roomid] = updateGame
-			room = updateRoom
-			game = updateGame
-		} else {
-			http.Redirect(w, r, "/FindAirplane/Game", http.StatusFound)
-			return
-		}
-	}
-
-	// Check if it is post or get
-	if r.Method == http.MethodPost {
-		//	if post, get new data, update it and then send the data back
-		updateGame := game.getUpdateGameData(r, userNow, room.boardLength)
-		airplaneGames[roomid] = updateGame
-	} else {
-		//	if get, then return data
-		updateGame := AirplaneGame{Board1: game.Board1, Board2: game.Board2, PlayerNow: game.PlayerNow, Round: game.Round, Win: game.Win, ThisPlayer: userNow}
-		b, err := json.Marshal(updateGame)
-		if err != nil {
-			println(err)
-		}
-		w.Write(b)
 	}
 }
 
