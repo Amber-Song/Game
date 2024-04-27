922f92399239ed9126e5e2ec927d8fd762e526b8
Vaughan Kitchen <contact@tophat.nz>
2024-04-27 12:13:42 +1200

    Fix plane icon

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +14,8 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueAxios, axios)
+
+library.add(fas)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
